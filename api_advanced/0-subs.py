3ca0389a617f463d497a4b7d3954e233430781ed
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2024-03-01 12:50:30 -0800

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""Doc"""
+"""Function that queries the Reddit API and \
+    returns the number of subscribbers of a given subreddit"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """"Doc"""
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My User Agent 1.0"}
+    response = requests.get(url, headers=headers)
 
-    res = requests.get(url,
-                       headers={
-                           'User-Agent': 'Mozilla/5.0'})
-
-    if res.status_code != 200:
+    if response.status_code != 200:
         return 0
     else:
-        json_response = res.json()
-        return json_response.get('data').get('subscribers')
+        return response.json().get("data").get("subscribers")
